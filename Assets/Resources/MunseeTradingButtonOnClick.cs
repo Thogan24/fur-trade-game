49e32c64fd9123f810eddd56ab11748fb967e457
Thogan24 <thogan1924@gmail.com>
2024-03-19 20:04:48 -0400

    Typo

--- Assets/Resources/MunseeTradingButtonOnClick.cs
+++ Assets/Resources/MunseeTradingButtonOnClick.cs
@@ -51,6 +51,7 @@ public class MunseeTradingButtonOnClick : MonoBehaviour
         
     }
 
+    [PunRPC]
     void teamsThatAreTrading(string userIDOfClicker)
     {
         gameManager = GameObject.FindGameObjectWithTag("Game Manager").GetComponent<GameManager>();
