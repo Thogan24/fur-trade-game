b6eee55da61280f095cc653eac766f5f1b69981f
matspe24 <91612588+matspe24@users.noreply.github.com>
2024-03-20 20:37:34 -0400

    testing wishlistcheck
    yea

--- Assets/Resources/CardOnClick.cs
+++ Assets/Resources/CardOnClick.cs
@@ -34,9 +34,49 @@ public class CardOnClick : MonoBehaviour
             string tag = this.gameObject.tag;
             string parentTag = this.gameObject.transform.parent.tag;
             Debug.Log(tag);
-            gameManager.gameObject.GetComponent<PhotonView>().RPC("addCardToTrade", RpcTarget.All, tag, parentTag);
+            if (CanWishlist)
+            {
+                gameManager.gameObject.GetComponent<PhotonView>().RPC("addCardToTrade", RpcTarget.All, tag, parentTag);
+            } else
+            {
+                StartCoroutine(FlashRed());
+            }
         }
-        
 
+    }
+
+    public bool CanWishlist()
+    {
+        string player_ID = PhotonNetwork.LocalPlayer.ToString();
+        if (player_ID == gameManager.Dutch)
+        {
+            int index = FindIndexOfTag(gameManager.tags, this.gameObject.tag);
+            Debug.Log(gameManager.PhilipsesAmounts[index]);
+        }
+        return true;
+    }
+
+    public int FindIndexOfTag(string[] arr, string str)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == str)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private IEnumerator FlashRedCoroutine()
+    {
+        private SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        Color originalColor = spriteRenderer.color;
+
+        spriteRenderer.color = new Color(1f, originalColor.g * 0.5f, originalColor.b * 0.5f, originalColor.a);
+
+        yield return new WaitForSeconds(1);
+
+        spriteRenderer.color = originalColor;
     }
 }
