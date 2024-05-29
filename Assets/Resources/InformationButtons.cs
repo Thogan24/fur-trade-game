c175e982bbbd740c81fe5474b86b97b5e73933d5
Thogan24 <thogan1924@gmail.com>
2024-05-29 14:55:40 -0400

    Can open and close respective description info

--- Assets/Resources/InformationButtons.cs
+++ Assets/Resources/InformationButtons.cs
@@ -6,11 +6,23 @@ using UnityEngine.UI;
 public class InformationButtons : MonoBehaviour
 {
     public GameObject imageObject;
+    public Sprite image;
     public void Start()
     {
-        imageObject = GameObject.FindGameObjectWithTag("DescriptionImage");
+        /*imageObject = GameObject.FindGameObjectWithTag("DescriptionImage");
         var newColor = new Color(1.0f, 1.0f, 1.0f, 0f);
         imageObject.GetComponent<Image>().color = newColor;
+
+
+        image = Resources.Load<Sprite>("SixNationsDescription");*/
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            closeInfo();
+        }
     }
     public void SceneChange()
     {
@@ -21,9 +33,45 @@ public class InformationButtons : MonoBehaviour
 
     
     public string[] tagArray = { "DutchInfo", "PhilipsesInfo", "SixNationsInfo", "MunseeInfo" };
-    public string[] imageDescriptionPathArray = { "SixNationsDescription" };
+    public string[] imageDescriptionTags = { "DutchDescription", "PhilipsesDescription", "SixNationsDescription", "MunseeDescription", };
     public int index;
+
     public void infoOnClicked()
+    {
+
+        Debug.Log("Clicked");
+        var newColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        for (int i = 0; i < tagArray.Length; i++)
+        {
+
+            if (tagArray[i] == this.gameObject.tag)
+            {
+
+                index = i;
+                Debug.Log(index);
+            }
+        }
+
+        GameObject.FindGameObjectWithTag(imageDescriptionTags[index]).gameObject.GetComponent<Image>().color = newColor;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*    public void infoOnClicked()
     {
         
         Debug.Log("Clicked");
@@ -44,14 +92,21 @@ public class InformationButtons : MonoBehaviour
             }
         }
         Debug.Log(Resources.Load<Sprite>("SixNationsDescription"));
-        imageObject.GetComponent<Image>().sprite = Resources.Load<Sprite>(imageDescriptionPathArray[index]) as Sprite;
+        image = Resources.Load<Sprite>(imageDescriptionPathArray[index]);
+        imageObject.GetComponent<Image>().sprite = image;
         
-    }
+    }*/
 
     public void closeInfo()
     {
-        var newColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
-        imageObject.GetComponent<Image>().color = newColor;
+        var newColor = new Color(1.0f, 1.0f, 1.0f, 0f);
+        for(int j = 0; j < imageDescriptionTags.Length; j++)
+        {
+            if(GameObject.FindGameObjectWithTag(imageDescriptionTags[j]) != null)
+            {
+                GameObject.FindGameObjectWithTag(imageDescriptionTags[j]).GetComponent<Image>().color = newColor;
+            }
+        }
     }
 
 }
