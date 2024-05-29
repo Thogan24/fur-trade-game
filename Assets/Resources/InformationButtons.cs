a9313f0d4f4639934090e539afe22a2392f92513
Thogan24 <thogan1924@gmail.com>
2024-05-29 15:25:30 -0400

    background to background

--- Assets/Resources/InformationButtons.cs
+++ Assets/Resources/InformationButtons.cs
@@ -32,7 +32,7 @@ public class InformationButtons : MonoBehaviour
         imageObject.GetComponent<Image>().color = newColor;
     }
 
-    
+
     public string[] tagArray = { "DutchInfo", "PhilipsesInfo", "SixNationsInfo", "MunseeInfo" };
     public string[] imageDescriptionTags = { "DutchDescription", "PhilipsesDescription", "SixNationsDescription", "MunseeDescription", };
     public int index;
@@ -52,14 +52,14 @@ public class InformationButtons : MonoBehaviour
                 Debug.Log(index);
             }
         }
-        if(SceneManager.GetActiveScene().name == "TeamSelect")
+        if (SceneManager.GetActiveScene().name == "TeamSelect")
         {
             GameObject.FindGameObjectWithTag(imageDescriptionTags[index]).gameObject.GetComponent<Image>().color = newColor;
         }
         else if (SceneManager.GetActiveScene().name == "Main_Scene")
         {
             GameObject[] descriptionArray = GameObject.FindGameObjectsWithTag(imageDescriptionTags[index]);
-            if(descriptionArray != null)
+            if (descriptionArray != null)
             {
                 for (int a = 0; a < descriptionArray.Length; a++)
                 {
@@ -67,7 +67,7 @@ public class InformationButtons : MonoBehaviour
                 }
             }
         }
-        
+
 
     }
 
@@ -86,37 +86,37 @@ public class InformationButtons : MonoBehaviour
 
 
 
-/*    public void infoOnClicked()
-    {
-        
-        Debug.Log("Clicked");
-        Debug.Log("tag is: " + gameObject.tag);
-
-        var newColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
-        imageObject.GetComponent<Image>().color = newColor;
-        //imageObject.gameObject.SetActive(true);
-        Debug.Log(gameObject.tag);
-        for (int i = 0; i < tagArray.Length; i++)
+    /*    public void infoOnClicked()
         {
-            
-            if(tagArray[i] == this.gameObject.tag)
+
+            Debug.Log("Clicked");
+            Debug.Log("tag is: " + gameObject.tag);
+
+            var newColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+            imageObject.GetComponent<Image>().color = newColor;
+            //imageObject.gameObject.SetActive(true);
+            Debug.Log(gameObject.tag);
+            for (int i = 0; i < tagArray.Length; i++)
             {
 
-                index = i;
-                Debug.Log(index);
+                if(tagArray[i] == this.gameObject.tag)
+                {
+
+                    index = i;
+                    Debug.Log(index);
+                }
             }
-        }
-        Debug.Log(Resources.Load<Sprite>("SixNationsDescription"));
-        image = Resources.Load<Sprite>(imageDescriptionPathArray[index]);
-        imageObject.GetComponent<Image>().sprite = image;
-        
-    }*/
+            Debug.Log(Resources.Load<Sprite>("SixNationsDescription"));
+            image = Resources.Load<Sprite>(imageDescriptionPathArray[index]);
+            imageObject.GetComponent<Image>().sprite = image;
+
+        }*/
 
     public void closeInfo()
     {
         var newColor = new Color(1.0f, 1.0f, 1.0f, 0f);
 
-        for(int j = 0; j < imageDescriptionTags.Length; j++)
+        for (int j = 0; j < imageDescriptionTags.Length; j++)
         {
             if (SceneManager.GetActiveScene().name == "TeamSelect")
             {
@@ -130,13 +130,14 @@ public class InformationButtons : MonoBehaviour
                 GameObject[] descriptionArray2 = GameObject.FindGameObjectsWithTag(imageDescriptionTags[j]);
                 for (int b = 0; b < descriptionArray2.Length; b++)
                 {
-                    if(descriptionArray2[b] != null)
+                    if (descriptionArray2[b] != null)
                     {
                         descriptionArray2[b].gameObject.GetComponent<Image>().color = newColor;
                     }
                 }
-                
-            }
-    }
 
+            }
+        }
+
+    }
 }
