7a0f9bf4e4de3bed3c523ce4280b3c09749ea624
Thogan24 <thogan1924@gmail.com>
2024-06-08 11:38:19 -0400

    debug

--- Assets/Resources/ClearTradeButtonOnClick.cs
+++ Assets/Resources/ClearTradeButtonOnClick.cs
@@ -22,19 +22,20 @@ public class ClearTradeButtonOnClick : MonoBehaviour
     public void ClearTradeButtonOnClicked()
     {
         Debug.Log("Clear Trade Button Clicked");
-        this.GetComponent<PhotonView>().RPC("WhenClicked", RpcTarget.All, PhotonNetwork.LocalPlayer.ToString());
-
+        gameManager = GameObject.FindGameObjectWithTag("Game Manager").GetComponent<GameManager>();
+        if (gameManager.turn == 1 && PhotonNetwork.LocalPlayer.ToString() == gameManager.Dutch || gameManager.turn == 2 && PhotonNetwork.LocalPlayer.ToString() == gameManager.Philipses || gameManager.turn == 3 && PhotonNetwork.LocalPlayer.ToString() == gameManager.SixNations || gameManager.turn == 4 && PhotonNetwork.LocalPlayer.ToString() == gameManager.Munsee)
+        {
+            this.GetComponent<PhotonView>().RPC("WhenClicked", RpcTarget.All, PhotonNetwork.LocalPlayer.ToString());
+        }
     }
 
     [PunRPC]
     void WhenClicked(string userIDOfClicker) // 
     {
         gameManager = GameObject.FindGameObjectWithTag("Game Manager").GetComponent<GameManager>();
-        if(gameManager.turn == 1 && PhotonNetwork.LocalPlayer.ToString() == gameManager.Dutch || gameManager.turn == 2 && PhotonNetwork.LocalPlayer.ToString() == gameManager.Philipses || gameManager.turn == 3 && PhotonNetwork.LocalPlayer.ToString() == gameManager.SixNations || gameManager.turn == 4 && PhotonNetwork.LocalPlayer.ToString() == gameManager.Munsee)
-        {
-            gameManager.clearTradeButton = true;
-            gameManager.GetComponent<PhotonView>().RPC("clearAllTrades", RpcTarget.All);
-        }
+        gameManager.clearTradeButton = true;
+        gameManager.GetComponent<PhotonView>().RPC("clearAllTrades", RpcTarget.All);
+        
         
     }
 }
