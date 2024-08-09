316942ccbcd17a3e9c4b20fcbdd0bdd6e750beed
Thogan24 <thogan1924@gmail.com>
2024-08-09 12:46:02 -0400

    solving messed up positions

--- Assets/Scripts/PlayerListingMenu.cs
+++ Assets/Scripts/PlayerListingMenu.cs
@@ -12,7 +12,7 @@ public class PlayerListingMenu : MonoBehaviourPunCallbacks
     private PlayerListing playerListing;
 
     private List<PlayerListing> playerList = new List<PlayerListing>();
-    public float changeInYFromListings = 57.5f;
+    public float changeInYFromListings = 57f;
     public int playerCount = 0;
 
     private void Awake()
@@ -26,16 +26,29 @@ public class PlayerListingMenu : MonoBehaviourPunCallbacks
         foreach(KeyValuePair<int, Player> playerInfo in PhotonNetwork.CurrentRoom.Players)
         {
             Debug.Log("This many players (count me)");
-            AddPlayerListing(playerInfo.Value);
+            AddPlayerListing(playerInfo.Value, true);
         }
 
     }
 
-    private void AddPlayerListing(Player player)
+    private void AddPlayerListing(Player player, bool notInRoomYet)
     {
         Debug.Log("Adding player listing");
         PlayerListing listing = Instantiate(playerListing, content); //new Vector3((float)727.2, 367 - PhotonNetwork.CountOfPlayers * 2, 0), Quaternion.identity
-        listing.transform.position = listing.transform.position + new Vector3(0, 0 - (changeInYFromListings * (playerCount)), 0);
+        Debug.Log(listing.transform.position.y);
+        if(notInRoomYet == true)
+        {
+            listing.transform.position = listing.transform.position + new Vector3(0.948288f + 1.525879e-05f, -2 - (38.6f * (playerCount)), 0);
+
+        }
+        else
+        {
+            listing.transform.position = listing.transform.position + new Vector3(0.948288f + 1.525879e-05f, -2 - (changeInYFromListings * (playerCount)), 0);
+
+        }
+        Debug.Log(playerCount);
+        Debug.Log(changeInYFromListings * (playerCount));
+        Debug.Log(listing.transform.position.y);
         playerCount++;
         if (listing != null)
         {
@@ -48,7 +61,7 @@ public class PlayerListingMenu : MonoBehaviourPunCallbacks
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
         Debug.Log("PLAYER ENTERED!!!");
-        AddPlayerListing(newPlayer);
+        AddPlayerListing(newPlayer, false);
     }
     public override void OnPlayerLeftRoom(Player otherPlayer)
     {
